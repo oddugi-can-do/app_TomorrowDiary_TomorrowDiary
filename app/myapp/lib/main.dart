21280bd04ed19ef02356a332f833d2087e6ef1ff
Jeong Jong In <chongin12@naver.com>
2021-09-13 11:11:52 +0000

    add calendar events and builders

--- app/myapp/lib/main.dart
+++ app/myapp/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'event.dart';
 
 void main() {
-  runApp(const MyApp());
+  initializeDateFormatting('ko_KR', null).then((_) => runApp(const MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -13,12 +15,23 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  CalendarFormat _calendarFormat = CalendarFormat.month;
+  late final ValueNotifier<List<Event>> _selectedEvents;
+  final CalendarFormat _calendarFormat = CalendarFormat.month;
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
+
   @override
   void initState() {
     super.initState();
+
+    _selectedDay = _focusedDay;
+    _selectedEvents = ValueNotifier(_getEventsForDay(_selectedDay!));
+  }
+
+  @override
+  void dispose() {
+    _selectedEvents.dispose();
+    super.dispose();
   }
 
   @override
@@ -32,30 +45,7 @@ class _MyAppState extends State<MyApp> {
         body: SingleChildScrollView(
           child: Column(
             children: [
-              TableCalendar(
-                focusedDay: _focusedDay,
-                firstDay: DateTime.utc(2020, 10, 19),
-                lastDay: DateTime.utc(2022, 4, 18),
-                calendarFormat: _calendarFormat,
-                rangeSelectionMode: RangeSelectionMode.disabled,
-                availableCalendarFormats: {CalendarFormat.month: ''},
-                selectedDayPredicate: (day) {
-                  return isSameDay(day, _selectedDay);
-                },
-                onDaySelected: (selectedDay, focusedDay) {
-                  if (!isSameDay(_selectedDay, selectedDay)) {
-                    // Call `setState()` when updating the selected day
-                    setState(() {
-                      _selectedDay = selectedDay;
-                      _focusedDay = focusedDay;
-                    });
-                  }
-                },
-                onPageChanged: (focusedDay) {
-                  // No need to call `setState()` here
-                  _focusedDay = focusedDay;
-                },
-              ),
+              _buildTableCalendar(),
               Padding(
                 padding: const EdgeInsets.all(20),
                 child: ElevatedButton(
@@ -77,6 +67,74 @@ class _MyAppState extends State<MyApp> {
           width: 200,
         ),
       ),
+    );
+  }
+
+  List<Event> _getEventsForDay(DateTime day) {
+    // Implementation example
+    return kEvents[day] ?? [];
+  }
+
+  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    if (!isSameDay(_selectedDay, selectedDay)) {
+      setState(() {
+        _selectedDay = selectedDay;
+        _focusedDay = focusedDay;
+      });
+
+      _selectedEvents.value = _getEventsForDay(selectedDay);
+    }
+  }
+
+  TableCalendar<Event> _buildTableCalendar() {
+    return TableCalendar(
+      calendarBuilders: calendarBuilder(),
+      focusedDay: _focusedDay,
+      firstDay: DateTime.utc(2020, 10, 19),
+      lastDay: DateTime.utc(2022, 4, 18),
+      calendarFormat: _calendarFormat,
+      eventLoader: _getEventsForDay,
+      rangeSelectionMode: RangeSelectionMode.disabled,
+      availableCalendarFormats: {CalendarFormat.month: ''},
+      calendarStyle: CalendarStyle(),
+      locale: 'ko_KR',
+      selectedDayPredicate: (day) {
+        return isSameDay(day, _selectedDay);
+      },
+      onDaySelected: (selectedDay, focusedDay) {
+        if (!isSameDay(_selectedDay, selectedDay)) {
+          // Call `setState()` when updating the selected day
+          setState(() {
+            _selectedDay = selectedDay;
+            _focusedDay = focusedDay;
+          });
+        }
+      },
+      onPageChanged: (focusedDay) {
+        // No need to call `setState()` here
+        _focusedDay = focusedDay;
+      },
+    );
+  }
+
+  CalendarBuilders<Event> calendarBuilder() {
+    return CalendarBuilders(
+      defaultBuilder: (context, day, focusedDay) {},
+      markerBuilder: (context, dateTime, events) {
+        final Widget child;
+        switch (events[0].eventState) {
+          case EventState.none:
+            child = Icon(Icons.star_border);
+            break;
+          case EventState.progressing:
+            child = Icon(Icons.star_half);
+            break;
+          case EventState.done:
+            child = Icon(Icons.star);
+            break;
+        }
+        return child;
+      },
     );
   }
 }
