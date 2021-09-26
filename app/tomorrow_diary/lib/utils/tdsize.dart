d02151387a030dc1d852131930c3d2bb0cebf212
Jeong Chong In <chongin12@naver.com>
2021-09-26 09:46:29 +0000

    tdsize 수정 및 utils.dart 에 export 추가

--- app/tomorrow_diary/lib/utils/tdsize.dart
+++ app/tomorrow_diary/lib/utils/tdsize.dart
@@ -1,9 +1,10 @@
 class TdSize {
   static const double xs = 6;
-  static const double s = 12;
-  static const double m = 15;
-  static const double l = 26;
-  static const double xl = 50;
+  static const double s = 12; // TextCategory.hint
+  static const double m = 15; // TextCategory.body
+  static const double l = 20; // TextCategory.header
+  static const double xl = 26;
+  static const double xxl = 30; // TextCategory.title
   static const double radiusS = 5;
   static const double radiusM = 10;
 }
