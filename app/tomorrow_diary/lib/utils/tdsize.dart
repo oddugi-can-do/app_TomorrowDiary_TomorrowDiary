56ccab5007eda82d8ff2f3dab7431471fdd9a327
Jeong Chong In <chongin12@naver.com>
2021-09-26 09:17:04 +0000

    size double로 다 바꿔주기

--- app/tomorrow_diary/lib/utils/tdsize.dart
+++ app/tomorrow_diary/lib/utils/tdsize.dart
@@ -1,9 +1,9 @@
 class TdSize {
-  static const xs = 6;
-  static const s = 12;
-  static const m = 15;
-  static const l = 26;
-  static const xl = 50;
-  static const radiusS = 5;
-  static const radiusM = 10;
+  static const double xs = 6;
+  static const double s = 12;
+  static const double m = 15;
+  static const double l = 26;
+  static const double xl = 50;
+  static const double radiusS = 5;
+  static const double radiusM = 10;
 }
