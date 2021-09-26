9374287fc0ef62905c03bcc2f3e74d2aa8151aef
Jeong Chong In <chongin12@naver.com>
2021-09-26 02:52:01 +0000

    add size and color

---
+++ app/tomorrow_diary/lib/utils/tdsize.dart
@@ -0,0 +1,9 @@
+class TdSize {
+  static const xs = 6;
+  static const s = 12;
+  static const m = 15;
+  static const l = 26;
+  static const xl = 50;
+  static const radiusS = 5;
+  static const radiusM = 10;
+}
