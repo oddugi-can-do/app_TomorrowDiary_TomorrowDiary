a6a398f710cc9c1a6bad0ee87597afd0b51b541f
sn0wd3er <69336817+sn0wd3er@users.noreply.github.com>
2021-10-16 23:04:40 +0900

    remove Aws Address

--- tomorrow_diary/lib/utils/connect_aws.dart
+++ tomorrow_diary/lib/utils/connect_aws.dart
@@ -1,7 +1,7 @@
   import 'package:http/http.dart' as http; //웹 땜시
   
 
-  const aws = 'https://vnq0k7mhr0.execute-api.ap-northeast-2.amazonaws.com/tomorrow/image';
+  const aws = '';
   Future<http.Response> httpPost(String bytes) {
   return http.post(
     Uri.parse(aws),
