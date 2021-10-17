4bbc4122057f504946294f1863800b6511f36231
Jeong Chong In <chongin12@naver.com>
2021-10-17 00:53:47 +0000

    aws 주소 뺌

--- tomorrow_diary/lib/utils/connect_aws.dart
+++ tomorrow_diary/lib/utils/connect_aws.dart
@@ -1,13 +1,12 @@
-  import 'package:http/http.dart' as http; //웹 땜시
-  
+import 'package:http/http.dart' as http; //웹 땜시
 
-  const aws = '';
-  Future<http.Response> httpPost(String bytes) {
+const aws = '';
+Future<http.Response> httpPost(String bytes) {
   return http.post(
     Uri.parse(aws),
     headers: <String, String>{
       'Content-Type': 'application/json; charset=UTF-8',
     },
-    body:bytes,
-    );
-  }
+    body: bytes,
+  );
+}
